873b160eb857982924a49cb9c62bd106e957dbe2
panchajanya <panchajanya@panchajanyamysarlas-MacBook-Pro.local>
2017-01-16 12:38:54 -0500

    more enhancements
    Added future weather prediction functionality. And improved current
    weather implementation. More Features yet to be developed soon

--- examples/myPyWit.py
+++ examples/myPyWit.py
@@ -1,6 +1,7 @@
 import sys
 import requests
 import json
+import datetime
 from wit import Wit
 
 if len(sys.argv) != 2:
@@ -28,10 +29,21 @@ def get_geolocation():
     j = json.loads(r.text)
     return j
 
-def get_forecast_from_open_weather_map(parameters):
+def get_current_forecast_from_open_weather_map(parameters):
     r = requests.get('http://api.openweathermap.org/data/2.5/weather',params=parameters)
     data = json.loads(r.text)
     return data['weather'][0]['description']
+
+def get_future_forecast_from_open_weather_map(owmDate,parameters):
+    futureWeatherForecast = ""
+    r = requests.get('http://api.openweathermap.org/data/2.5/forecast',params=parameters)
+    data = json.loads(r.text)
+    for i in data['list']:
+        if i['dt_txt'].find(owmDate) != -1:
+            simpleTime = datetime.datetime.strptime(i['dt_txt'], '%Y-%m-%d %H:%M:%S').strftime('%H:%M')
+            futureWeatherForecast += "%s at %s " %(i['weather'][0]['description'],str(simpleTime))
+
+    return futureWeatherForecast
 
 def get_forecast(request):
     context = request['context']
@@ -39,21 +51,35 @@ def get_forecast(request):
 
     loc = first_entity_value(entities, 'location')
     dateTime = first_entity_value(entities,'datetime')
-
+    intent = first_entity_value(entities,'intent')
+    if intent:
+        print(intent)
     if dateTime:
+        owmDate = datetime.datetime.strptime(dateTime[:-6], '%Y-%m-%dT%H:%M:%S.%f').strftime('%Y-%m-%d')
+        todaysDate = datetime.datetime.now()
         j = get_geolocation()
         latitude = j['latitude']
         longitude = j['longitude']
         parameters = {'lat':latitude,'lon':longitude,'appid':'f12f195956d3533f7a8d78665cd69def'}
-        context['futureForecast'] = get_forecast_from_open_weather_map(parameters)
-        #context['futureForecast'] = 'windy'
+        if str(todaysDate).find(owmDate) != -1:
+            context['futureForecast'] = get_current_forecast_from_open_weather_map(parameters)
+        else:
+            context['futureForecast'] = get_future_forecast_from_open_weather_map(owmDate,parameters)
+
+
 
     if loc and dateTime:
-        context['futureForecastLoc'] = 'Rainy at Paris'
+        owmDate = datetime.datetime.strptime(dateTime[:-6], '%Y-%m-%dT%H:%M:%S.%f').strftime('%Y-%m-%d')
+        todaysDate = datetime.datetime.now()
+        parameters = {'q':loc,'appid':'f12f195956d3533f7a8d78665cd69def'}
+        if str(todaysDate).find(owmDate) != -1:
+            context['futureForecast'] = get_current_forecast_from_open_weather_map(parameters)
+        else:
+            context['futureForecastLoc'] = get_future_forecast_from_open_weather_map(owmDate,parameters)
 
     if loc:
         parameters = {'q':loc,'appid':'f12f195956d3533f7a8d78665cd69def'}
-        context['forecast'] = get_forecast_from_open_weather_map(parameters)
+        context['forecast'] = get_current_forecast_from_open_weather_map(parameters)
          #context['forecast']= 'sunny'
         if context.get('missingLocation') is not None:
             del context['missingLocation']
